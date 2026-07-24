7ea644a92887ba2baf19ffd5830d338bcea7c5a0
NuWhE3wn0D <07805866.911aa@gmail.com>
2026-07-24 03:12:01 +0200

    Update implement utility functions for data handling

--- utils.py
+++ utils.py
@@ -5,26 +5,27 @@ def load_json(file_path):
         return json.load(file)
 
 
-def save_json(data, file_path):
+def save_json(file_path, data):
     with open(file_path, 'w') as file:
         json.dump(data, file, indent=4)
 
 
-def merge_dictionaries(*dicts):
-    merged = {}
-    for dictionary in dicts:
-        merged.update(dictionary)
-    return merged
+def merge_dicts(dict1, dict2):
+    result = dict1.copy()
+    result.update(dict2)
+    return result
 
 
-def filter_keys(data, keys):
-    return {key: data[key] for key in keys if key in data}
+def flatten_list(nested_list):
+    return [item for sublist in nested_list for item in sublist]  
 
 
-def validate_json_schema(data, schema):
-    from jsonschema import validate, ValidationError
-    try:
-        validate(instance=data, schema=schema)
-    except ValidationError as e:
-        return e.message
-    return True
+def find_key_in_dict(target_dict, target_key):
+    if target_key in target_dict:
+        return target_dict[target_key]
+    for key, value in target_dict.items():
+        if isinstance(value, dict):
+            found = find_key_in_dict(value, target_key)
+            if found:
+                return found
+    return None
