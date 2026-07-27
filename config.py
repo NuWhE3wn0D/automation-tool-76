9b8c10741fb444f5fdf04d93918d39bd055793dc
NuWhE3wn0D <07805866.911aa@gmail.com>
2026-07-27 06:54:13 +0200

    Improve implement config loader with defaults

--- config.py
+++ config.py
@@ -1,22 +1,15 @@
 import json
-import os
 
-class ConfigLoader:
-    def __init__(self, default_config_path, user_config_path):
-        self.default_config = self.load_config(default_config_path)
-        self.user_config = self.load_config(user_config_path)
+DEFAULT_CONFIG = {
+    'setting1': 'value1',
+    'setting2': True,
+    'setting3': 10
+}
 
-    def load_config(self, path):
-        if os.path.exists(path):
-            with open(path, 'r') as f:
-                return json.load(f)
-        return {}
-
-    def get_config(self):
-        config = self.default_config.copy()
-        config.update(self.user_config)
-        return config
-
-if __name__ == '__main__':
-    loader = ConfigLoader('defaults.json', 'user_config.json')
-    print(loader.get_config())
+def load_config(file_path='config.json'):
+    try:
+        with open(file_path, 'r') as file:
+            config = json.load(file)
+    except (FileNotFoundError, json.JSONDecodeError):
+        return DEFAULT_CONFIG
+    return {**DEFAULT_CONFIG, **config}
