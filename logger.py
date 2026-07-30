8e4d598568a14cfc355775682cef7fe855af9dc2
NuWhE3wn0D <07805866.911aa@gmail.com>
2026-07-30 03:11:57 +0200

    Refactor input validation in main processing loop

--- logger.py
+++ logger.py
@@ -1,26 +1,31 @@
 import logging
-from logging.handlers import RotatingFileHandler
 
-class Logger:
-    def __init__(self, name, log_file, max_bytes=10**6, backup_count=5):
+class CustomLogger:
+    def __init__(self, name):
         self.logger = logging.getLogger(name)
         self.logger.setLevel(logging.DEBUG)
-        handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.DEBUG)
         formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-        self.logger.addHandler(handler)
+        ch.setFormatter(formatter)
+        self.logger.addHandler(ch)
 
-    def debug(self, msg):
-        self.logger.debug(msg)
+    def debug(self, message):
+        self.logger.debug(message)
 
-    def info(self, msg):
-        self.logger.info(msg)
+    def info(self, message):
+        self.logger.info(message)
 
-    def warning(self, msg):
-        self.logger.warning(msg)
+    def warning(self, message):
+        self.logger.warning(message)
 
-    def error(self, msg):
-        self.logger.error(msg)
+    def error(self, message):
+        self.logger.error(message)
 
-    def critical(self, msg):
-        self.logger.critical(msg)
+    def critical(self, message):
+        self.logger.critical(message)
+
+logger = CustomLogger(__name__)
+
+if __name__ == '__main__':
+    logger.info('Logger initialized')
