0512dfeec29232fdeea6c40bea8008b952c4f355
Julia Brandt <07805866.911aa@gmail.com>
2026-08-16 15:17:10 +0200

    Improve memoization for fibonacci function

--- helpers.py
+++ helpers.py
@@ -1,15 +1,27 @@
-import time
-import requests
-from requests.exceptions import RequestException
+def memoize(func):
+    cache = {}
+    def memoized(*args):
+        if args not in cache:
+            cache[args] = func(*args)
+        return cache[args]
+    return memoized
 
-def retry_request(method, url, retries=3, backoff=1, **kwargs):
-    for attempt in range(retries):
-        try:
-            response = requests.request(method, url, **kwargs)
-            response.raise_for_status()
-            return response
-        except RequestException as e:
-            if attempt < retries - 1:
-                time.sleep(backoff * (2 ** attempt))
-            else:
-                raise e
+@memoize
+def fibonacci(n):
+    if n < 2:
+        return n
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+def batch_process(items, function, batch_size=10):
+    for i in range(0, len(items), batch_size):
+        yield function(items[i:i + batch_size])
+
+def parallel_process(func, iterable):
+    from concurrent.futures import ThreadPoolExecutor
+    with ThreadPoolExecutor() as executor:
+        return list(executor.map(func, iterable))
+
+if __name__ == '__main__':
+    print(fibonacci(10))
+    print(list(batch_process(range(100), sum)))
+    print(parallel_process(lambda x: x * 2, range(10)))
