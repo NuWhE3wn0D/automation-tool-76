76829142f296e3c701bf9cbecbe1b675f304f58a
Julia Brandt <07805866.911aa@gmail.com>
2026-08-18 15:15:23 +0200

    Improve type annotations for utils

--- utils.py
+++ utils.py
@@ -1,32 +1,28 @@
-import json
-
-def load_json(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        return json.load(file)
+from typing import List, Dict
 
 
-def save_json(data, file_path):
-    with open(file_path, 'w', encoding='utf-8') as file:
-        json.dump(data, file, ensure_ascii=False, indent=4)
+def calculate_average(numbers: List[float]) -> float:
+    """Calculate the average of a list of numbers."""
+    if not numbers:
+        return 0.0
+    return sum(numbers) / len(numbers)
 
 
-def merge_dicts(*dicts):
-    result = {}
-    for d in dicts:
-        result.update(d)
-    return result
+def filter_dictionary(data: Dict[str, int], threshold: int) -> Dict[str, int]:
+    """Filter a dictionary by a threshold value."""
+    return {key: value for key, value in data.items() if value > threshold}
 
 
-def filter_dict_keys(data_dict, keys):
-    return {key: data_dict[key] for key in keys if key in data_dict}
+def format_string(template: str, **kwargs: Dict[str, str]) -> str:
+    """Format a string with given keyword arguments."""
+    return template.format(**kwargs)
 
 
-def flatten_dict(nested_dict, parent_key='', sep='_'):
-    items = []
-    for key, value in nested_dict.items():
-        new_key = f'{parent_key}{sep}{key}' if parent_key else key
-        if isinstance(value, dict):
-            items.extend(flatten_dict(value, new_key, sep=sep).items())
-        else:
-            items.append((new_key, value))
-    return dict(items)
+def merge_lists(list1: List[int], list2: List[int]) -> List[int]:
+    """Merge two lists into one, avoiding duplicates."""
+    return list(set(list1 + list2))
+
+
+def get_unique_elements(elements: List[int]) -> List[int]:
+    """Return a list of unique elements from the input list."""
+    return list(set(elements))
