659d547552a434ce34c07e34474b8c38d578080e
Julia Brandt <07805866.911aa@gmail.com>
2026-09-11 15:05:34 +0200

    Refactor memoization decorator for intensive computation tasks

--- handler.py
+++ handler.py
@@ -1,24 +1,32 @@
-import time
 import functools
-from typing import Callable, Any, Type, Tuple
+from typing import Any, Callable, Dict
 
-def retry(exceptions: Tuple[Type[Exception], ...], tries: int = 3, delay: float = 1.0, backoff: float = 2.0):
-    def decorator(func: Callable):
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs) -> Any:
-            mtries, mdelay = tries, delay
-            while mtries > 1:
-                try:
-                    return func(*args, **kwargs)
-                except exceptions:
-                    time.sleep(mdelay)
-                    mtries -= 1
-                    mdelay *= backoff
-            return func(*args, **kwargs)
-        return wrapper
-    return decorator
+CACHE: Dict[tuple, Any] = {}
 
-@retry(ConnectionError, tries=3, delay=2)
-def fetch_data(url: str) -> str:
-    # Simulate network request
-    return f"data from {url}"
+def memoize(func: Callable) -> Callable:
+    @functools.wraps(func)
+    def wrapper(*args: Any, **kwargs: Any) -> Any:
+        key = (func.__name__, args, frozenset(kwargs.items()))
+        if key not in CACHE:
+            CACHE[key] = func(*args, **kwargs)
+        return CACHE[key]
+    return wrapper
+
+class DataHandler:
+    def __init__(self, data: list):
+        self._data = data
+
+    @memoize
+    def process_heavy_computation(self, factor: int) -> list:
+        return [x * factor for x in self._data]
+
+    def batch_update(self, factor: int, iterations: int) -> None:
+        for _ in range(iterations):
+            self.process_heavy_computation(factor)
+
+def clear_cache() -> None:
+    CACHE.clear()
+
+if __name__ == '__main__':
+    handler = DataHandler(list(range(1000)))
+    handler.batch_update(2, 500)
