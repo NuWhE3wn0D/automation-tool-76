271393179ebba6a13968583136243d06546015fd
Julia Brandt <07805866.911aa@gmail.com>
2026-09-23 15:06:16 +0200

    Optimize retry logic for network operations

--- utils.py
+++ utils.py
@@ -1,28 +1,22 @@
 import time
-from functools import wraps
-from typing import Callable, Tuple, Type, Any
+import functools
+from typing import Callable, Any, Type, Tuple
 
-
-def retry(
-    exceptions: Tuple[Type[BaseException], ...] = (Exception,),
-    tries: int = 3,
-    delay: float = 1.0,
-    backoff: float = 2.0,
-) -> Callable:
+def retry(exceptions: Tuple[Type[Exception], ...] = (Exception,), 
+          tries: int = 3, 
+          delay: float = 1.0, 
+          backoff: float = 2.0) -> Callable:
     def decorator(func: Callable) -> Callable:
-        @wraps(func)
+        @functools.wraps(func)
         def wrapper(*args: Any, **kwargs: Any) -> Any:
-            attempt_tries = tries
-            attempt_delay = delay
-            while attempt_tries > 0:
+            mtries, mdelay = tries, delay
+            while mtries > 1:
                 try:
                     return func(*args, **kwargs)
-                except exceptions as e:
-                    attempt_tries -= 1
-                    if attempt_tries == 0:
-                        raise e
-                    time.sleep(attempt_delay)
-                    attempt_delay *= backoff
+                except exceptions:
+                    time.sleep(mdelay)
+                    mtries -= 1
+                    mdelay *= backoff
             return func(*args, **kwargs)
         return wrapper
     return decorator
